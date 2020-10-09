65df512b9c308d12baf6eab05102d9f27b1dd6a2
aliamk <alia.azarian@gmail.com>
2020-10-09 16:16:21 +0100

    basic test

--- deno.js
+++ deno.js
@@ -1,13 +1,23 @@
-// function a() {
-//   console.log('THIS IS A TEST')
-// }
-
-// a()
-
-const food = 'love'
-
-if (food === 'love') {
-  console.log('🦕...Deno is born!')
-} else {
-  console.log('🥚...this egg needs some love')
+/* function a() {
+  console.log('THIS IS A TEST')
 }
+
+a()*/
+
+// const food = 'love'
+
+const food = Deno.args[0]
+// const mood = Deno.args[0]
+const parent = Deno.args[1]
+
+if (food === 'love' && parent === 'ryan') {
+  console.log('🦕 ...Deno1 is born!')
+} else {
+  console.log('🥚 ...this egg1 needs some love')
+}
+
+// if (mood === 'happy') {
+//   console.log('🦕 ...Deno2 is born!')
+// } else {
+//   console.log('🥚 ...this egg2 needs some love')
+// }
